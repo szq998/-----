81ea6dc4ee6ba162ad3e1d4d50c9258bb55b9abc
szq <szq001@foxmail.com>
2021-05-11 22:11:26 +0800

    better error handling

--- scripts/get-tieba-post.js
+++ scripts/get-tieba-post.js
@@ -1,3 +1,5 @@
+const DEBUG = true;
+
 async function fetchTiebaHtml(tiebaName) {
     const tiebaURL = `https://tieba.baidu.com/f?kw=${encodeURIComponent(
         tiebaName
@@ -46,47 +48,71 @@ async function getTiebaPostInfo(tiebaName) {
     tiebaHtml = tiebaHtml.replace(topPostClassRegex, '');
 
     // 用于匹配包含但个贴子所有信息的原始html
-    const rawPostRegex = /<li[^>]*class="[^">]*j_thread_list[^">]*"[^>]*>(.*?)最后回复人:/gs;
+    const rawPostRegex =
+        /<li[^>]*class="[^">]*j_thread_list[^">]*"[^>]*>(.*?)最后回复人:/gs;
     // 用于匹配贴子的各项信息
-    const linkAndTitleRegex = /<a[^>]*href="(?<relLink>[^">]+)"[^>]*class="[^>"]*j_th_tit[^>"]*"[^>]*>(?<rawTitle>[^<]+?)</s;
+    const linkAndTitleRegex =
+        /<a[^>]*href="(?<relLink>[^">]+)"[^>]*title="(?<rawTitle>[^"]*)"[^>]*class="[^>"]*j_th_tit[^>"]*"[^>]*>/s;
     // const authorRegex = /<span[^>]*title="主题作者:\s(?<author>[^>]+?)"[^>]*>/s;
-    const abstractRegex = /<div[^>]*class="[^">]*threadlist_abs_onlyline[^">]*"[^>]*>(?<rawAbstract>[^<]*?)</s;
+    const abstractRegex =
+        /<div[^>]*class="[^">]*threadlist_abs_onlyline[^">]*"[^>]*>(?<rawAbstract>[^<]*?)</s;
     const imgRegex = /<img[^>]*data-original="(?<imgUrl>[^"]+?)"/gs;
-    const imgVideoCoverRegex = /<div[^>]*class="[^">]*threadlist_video[^">]*"[^>]*><img src="(?<imgUrl>[^"]+)"/s;
+    const imgVideoCoverRegex =
+        /<div[^>]*class="[^">]*threadlist_video[^">]*"[^>]*><img src="(?<imgUrl>[^"]+)"/s;
 
     const info = [];
+    const errors = [];
     let execArr;
     while ((execArr = rawPostRegex.exec(tiebaHtml)) !== null) {
         const rawPost = execArr[1];
-        // 获取标题和贴子链接
-        const { rawTitle, relLink } = rawPost.match(linkAndTitleRegex).groups;
-        // 去掉html字符实体
-        const title = htmlEntityReplace(rawTitle);
-        const link = 'https://tieba.baidu.com' + relLink;
-        // 获取贴子作者
-        // const { author } = rawPost.match(authorRegex);
-        // 获取贴子摘要
-        const { rawAbstract } = rawPost.match(abstractRegex)?.groups ?? {};
-        let abstract = null;
-        if (rawAbstract) {
-            abstract = rawAbstract.trim();
+        try {
+            // 获取标题和贴子链接
+            const { rawTitle, relLink } =
+                rawPost.match(linkAndTitleRegex).groups;
             // 去掉html字符实体
-            abstract = htmlEntityReplace(abstract);
-            abstract = abstract.length ? abstract : null;
-        }
-        // 获取贴子封面照片
-        const imgUrls = []; // 可能没有图片
-        for (const match of rawPost.matchAll(imgRegex)) {
-            imgUrls.push(match.groups.imgUrl);
-        }
-        // 获取视频的封面
-        const { imgUrl: imgVideoCoverUrl } =
-            rawPost.match(imgVideoCoverRegex)?.groups ?? {};
-        if (imgVideoCoverUrl) {
-            imgUrls.push(imgVideoCoverUrl);
-        }
+            const title = htmlEntityReplace(rawTitle);
+            const link = 'https://tieba.baidu.com' + relLink;
+            // 获取贴子作者
+            // const { author } = rawPost.match(authorRegex);
+            // 获取贴子摘要
+            const { rawAbstract } = rawPost.match(abstractRegex)?.groups ?? {};
+            let abstract = null;
+            if (rawAbstract) {
+                abstract = rawAbstract.trim();
+                // 去掉html字符实体
+                abstract = htmlEntityReplace(abstract);
+                abstract = abstract.length ? abstract : null;
+            }
+            // 获取贴子封面照片
+            const imgUrls = []; // 可能没有图片
+            for (const match of rawPost.matchAll(imgRegex)) {
+                imgUrls.push(match.groups.imgUrl);
+            }
+            // 获取视频的封面
+            const { imgUrl: imgVideoCoverUrl } =
+                rawPost.match(imgVideoCoverRegex)?.groups ?? {};
+            if (imgVideoCoverUrl) {
+                imgUrls.push(imgVideoCoverUrl);
+            }
 
-        info.push({ title, link, abstract, imgUrls /* author,*/ });
+            info.push({ title, link, abstract, imgUrls /* author,*/ });
+        } catch (err) {
+            if (DEBUG) {
+                console.log(rawPost);
+                console.error(err);
+                errors.push({
+                    error: { message: err.message, stack: err.stack },
+                    rawPost,
+                });
+            }
+            continue;
+        }
+    }
+    if (DEBUG && errors.length) {
+        $file.write({
+            data: $data({ string: JSON.stringify(errors) }),
+            path: `assets/log/${tiebaName}-${new Date()}.json`,
+        });
     }
     return info.length ? info : null;
 }
