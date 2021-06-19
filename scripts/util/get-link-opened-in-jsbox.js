8d04cd59cdd4cc28f9bc6990cd853785236ddda1
szq998 <szq001@foxmail.com>
2021-06-19 16:53:36 +0800

    $safari.open前，首先退回到rootViewController

--- scripts/util/get-link-opened-in-jsbox.js
+++ scripts/util/get-link-opened-in-jsbox.js
@@ -1,5 +1,5 @@
 function getLinkOpenedInJSBox(url) {
-    const script = `$safari.open('${url}')`;
+    const script = `$objc("UIApplication").$sharedApplication().$delegate().$window().$rootViewController().$dismissViewControllerAnimated_completion(true, $block('void, void',()=>{$safari.open('${url}')}))`;
     return `jsbox://run?script=${encodeURIComponent(script)}`;
 }
 
